368c25b69aa5978857f4ff79cc3e99f62b9beb45
RAJ RABADIYA <rajrabadiya.018@gmail.com>
2024-12-08 17:31:15 +0530

    Time: 44 ms (95.04%), Space: 124.4 MB (85.57%) - LeetHub

--- 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.cpp
@@ -1,22 +1,42 @@
 class Solution {
 public:
     int maxTwoEvents(vector<vector<int>>& events) {
-        sort(events.begin(), events.end());
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
-        int maxVal = 0, ans = 0;
-
-        for (const auto& event : events) {
-            int start = event[0], end = event[1], value = event[2];
-
-            while (!pq.empty() && pq.top().first < start) {
-                maxVal = max(maxVal, pq.top().second);
-                pq.pop();
-            }
-
-            ans = max(ans, maxVal + value);
-            pq.push({end, value});
+        int n = events.size();
+        
+        sort(events.begin(), events.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[0] < b[0];
+        });
+        
+        vector<int> suffixMax(n);
+        suffixMax[n - 1] = events[n - 1][2];
+        
+        for (int i = n - 2; i >= 0; --i) {
+            suffixMax[i] = max(events[i][2], suffixMax[i + 1]);
         }
-
-        return ans;
+        
+        int maxSum = 0;
+        
+        for (int i = 0; i < n; ++i) {
+            int left = i + 1, right = n - 1;
+            int nextEventIndex = -1;
+            
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (events[mid][0] > events[i][1]) {
+                    nextEventIndex = mid;
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            
+            if (nextEventIndex != -1) {
+                maxSum = max(maxSum, events[i][2] + suffixMax[nextEventIndex]);
+            }
+            
+            maxSum = max(maxSum, events[i][2]);
+        }
+        
+        return maxSum;
     }
 };
