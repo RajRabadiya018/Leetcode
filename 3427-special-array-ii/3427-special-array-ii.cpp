117166a8c03ab13b4c195155cd7ec0af8340e937
RAJ RABADIYA <rajrabadiya.018@gmail.com>
2024-12-09 18:13:07 +0530

    Time: 2047 ms (5.47%), Space: 126.1 MB (76.85%) - LeetHub

--- 3427-special-array-ii/3427-special-array-ii.cpp
+++ 3427-special-array-ii/3427-special-array-ii.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size();
-        vector<int> prefix(n, 0); 
-        
-        // Calculating prefix array
-        for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i - 1];
-            if ((nums[i - 1] % 2 == 0 && nums[i] % 2 == 0) || (nums[i - 1] % 2 != 0 && nums[i] % 2 != 0)) {
-                prefix[i]++;
+        vector<int> v;
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if ((nums[i] % 2 == 0 && nums[i + 1] % 2 == 0) || (nums[i] % 2 != 0 && nums[i + 1] % 2 != 0)) {
+                v.push_back(i);
             }
         }
-
-        vector<bool> ans; 
-        
- 
-        for (auto& q : queries) {
-            int left = q[0], right = q[1];
-            int specialCount = prefix[right] - (left > 0 ? prefix[left] : 0);
-            ans.push_back(specialCount == 0);
+        vector<bool> ans;
+        for (int i = 0; i < queries.size(); i++) {
+            int s = queries[i][0];
+            int e = queries[i][1];
+            bool found = false;
+            for (auto it : v) {
+                if (it >= e) {
+                    ans.push_back(true);
+                    found = true;
+                    break;
+                }
+                if (it < e && it >= s) {
+                    ans.push_back(false);
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                ans.push_back(true);
+            }
         }
-
-        return ans; 
+        return ans;
     }
 };
