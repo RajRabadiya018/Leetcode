c0a22653c544a90f9faaf0de7e52193f8699acd0
RAJ RABADIYA <rajrabadiya.018@gmail.com>
2024-12-31 15:03:32 +0530

    Time: 0 ms (100%), Space: 13.5 MB (23.42%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -3,24 +3,24 @@ public:
     int t[366];
     int memoized(vector<int>& days, vector<int>& costs, int& n, int idx) {
         if(idx >= n)
-            return 0; //you can't travel, so no cost
+            return 0; //we can't travel, so no cost
                 
         if(t[idx] != -1)
             return t[idx];
         
-        //if i take 1-day pass at idx
+        //take 1-day pass at idx
         int cost_1 = costs[0] + memoized(days, costs, n, idx+1);
         
         
-        //if i take 7-day pass at idx
-        int i          = idx;
+        //take 7-day pass at idx
+        int i = idx;
         while(i < n && days[i] < days[idx]+7) {
             i++;
         }
         int cost_7 = costs[1] + memoized(days, costs, n, i);
         
         
-        //if i take 30-day pass at idx
+        //take 30-day pass at idx
         int j      = idx;
         while(j < n && days[j] < days[idx]+30) {
             j++;
