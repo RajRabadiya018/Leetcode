1e5d261932e7b0bafb1ed76f50372de437d87ca6
RAJ RABADIYA <rajrabadiya.018@gmail.com>
2026-01-15 17:55:48 +0530

    Time: 1 ms (50.44%), Space: 58.3 MB (45.91%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let i = 1;
+    for(let j = 0; j<nums.length; j++){
+        if(nums[j]!==nums[i-1]){
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
+};
