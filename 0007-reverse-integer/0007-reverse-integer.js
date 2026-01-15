8e607915c765ac0b14403a7f8289778184cb3a9f
RAJ RABADIYA <rajrabadiya.018@gmail.com>
2026-01-15 17:32:44 +0530

    Time: 53 ms (55.68%), Space: 55.7 MB (64.58%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -2,21 +2,13 @@
  * @param {number} x
  * @return {number}
  */
-var reverse = function(number) {
+var reverse = function(x) {
+    let absV = Math.abs(x);
+    let absR = absV.toString().split('').reverse().join('');
 
-let temp = Math.abs(number);
-let n = 0;
-while(temp>0){
-    let last = temp%10;
-    n = n*10 + last;
-    temp = Math.floor(temp/10);
-}
-//if(number < 0) return n *=-1;
+    if(absR > 2**31) return 0;
 
-    if (n > 2**31) {
-        return 0;
-    }
-    
-return n * Math.sign(number);
+    return absR * Math.sign(x);
+
 
 };
